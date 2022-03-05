17cfc422cd85b3d3e7699ad8879357c1ec65d7f9
wenshuoliu <wenshuoliu@tencent.com>
2022-03-05 13:14:27 +0800

    feat:init

---
+++ src/main/java/com/loveunited/tmall_b_backend/TmallBBackendApplication.java
@@ -0,0 +1,13 @@
+package com.loveunited.tmall_b_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmallBBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmallBBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loveunited/tmall_b_backend/TmallBBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loveunited.tmall_b_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmallBBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
