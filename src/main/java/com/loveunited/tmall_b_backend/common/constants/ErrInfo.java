2a2e6a7c53da3f461788db3e98733f618aa30183
wenshuoliu <wenshuoliu@tencent.com>
2022-03-18 21:59:22 +0800

    feat:更新删除条件

--- src/main/java/com/loveunited/tmall_b_backend/common/constants/ErrInfo.java
+++ src/main/java/com/loveunited/tmall_b_backend/common/constants/ErrInfo.java
@@ -17,25 +17,27 @@ public enum ErrInfo {
     INSERT_CATEGORY_NAME_EXISTS(1004, "品类名已存在"),
     DELETE_CATEGORY_FAILED(1005, "删除失败，请先删除其所有子品类"),
     CATEGORY_ID_NOT_EXISTS(1006, "当前品类ID不存在"),
+    DELETE_CATEGORY_FAILED_COMMODITY_BIND(1007, "删除品类失败，有商品绑定到该品类"),
 
     // 品牌相关
     BRAND_ID_NOT_EXISTS(1007, "当前品牌ID不存在"),
     INSERT_BRAND_NAME_EXISTS(1008, "待新增的品牌名已存在"),
+    DELETE_BRAND_ERROR_COMMODITY_BIND(1009, "当前品牌下有绑定的商品，无法删除"),
 
     // 商品相关
-    COMMODITY_ID_NOT_EXISTS(1009, "当前商品ID不存在"),
-    COMMODITY_ALREADY_ON_SALE(1010, "商品已经上架了"),
-    COMMODITY_STATUS_ERROR(1011, "请先上架商品或商品已经被下架"),
-    COMMODITY_PROP_FORMAT_ERROR(1012, "商品属性的json格式错误,需要是String-String的map形式"),
+    COMMODITY_ID_NOT_EXISTS(1010, "当前商品ID不存在"),
+    COMMODITY_ALREADY_ON_SALE(1011, "商品已经上架了"),
+    COMMODITY_STATUS_ERROR(1012, "请先上架商品或商品已经被下架"),
+    COMMODITY_PROP_FORMAT_ERROR(1013, "商品属性的json格式错误,需要是String-String的map形式"),
 
     // 订单相关
-    ORDERINFO_DETAIL_FORMAT_ERROR(1013, "订单详情格式错误,请检查"),
-    ORDERINFO_ID_NOT_EXISTS(1014, "订单ID不存在"),
-    ORDERINFO_DETAIL_INVENTORY_INFINITY(1015, "商品库存不足"),
-    ORDERINFO_DETAIL_CONTAINS_COMM_NOT_EXISTS(1016, "订单中的商品ID不存在"),
+    ORDERINFO_DETAIL_FORMAT_ERROR(1014, "订单详情格式错误,请检查"),
+    ORDERINFO_ID_NOT_EXISTS(1015, "订单ID不存在"),
+    ORDERINFO_DETAIL_INVENTORY_INFINITY(1016, "商品库存不足"),
+    ORDERINFO_DETAIL_CONTAINS_COMM_NOT_EXISTS(1017, "订单中的商品ID不存在"),
 
     // 退货单相关
-    BACKORDERINFO_ID_NOT_EXISTS(1016, "退货单ID不存在"),
+    BACKORDERINFO_ID_NOT_EXISTS(1018, "退货单ID不存在"),
 
     // 通用错误码
     PARAMETER_ERROR(10000, "参数不合法"),
