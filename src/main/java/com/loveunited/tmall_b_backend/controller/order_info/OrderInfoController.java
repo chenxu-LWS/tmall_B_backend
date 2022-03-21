6501ee05d6c0d773be1e3ecdc49859fd407a54a8
wenshuoliu <wenshuoliu@tencent.com>
2022-03-21 15:23:16 +0800

    feat:删除直接新增订单的接口

--- src/main/java/com/loveunited/tmall_b_backend/controller/order_info/OrderInfoController.java
+++ src/main/java/com/loveunited/tmall_b_backend/controller/order_info/OrderInfoController.java
@@ -1,7 +1,5 @@
 package com.loveunited.tmall_b_backend.controller.order_info;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,13 +7,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
 import com.loveunited.tmall_b_backend.common.ReturnObject;
 import com.loveunited.tmall_b_backend.common.ReturnPageObject;
 import com.loveunited.tmall_b_backend.common.constants.ErrInfo;
 import com.loveunited.tmall_b_backend.common.exception.BizException;
 import com.loveunited.tmall_b_backend.common.page.PageBean;
-import com.loveunited.tmall_b_backend.controller.order_info.dto.InsertOrderInfoDTO;
 import com.loveunited.tmall_b_backend.controller.order_info.dto.QueryOrderInfoByCustomerNameByPageDTO;
 import com.loveunited.tmall_b_backend.service.commodity.CommodityService;
 import com.loveunited.tmall_b_backend.service.order_info.OrderInfoService;
@@ -53,39 +49,5 @@ public class OrderInfoController {
         final PageBean<OrderInfoDTO> orderInfoDTOPageBean = orderInfoService
                 .queryOrderInfoByCustomerNameByPage(dto.getCustomerName(), dto.getPageNo(), dto.getPageSize());
         return new ReturnPageObject<>(true, orderInfoDTOPageBean, 0);
-    }
-
-    @RequestMapping("/insert")
-    @ResponseBody
-    public ReturnObject insertOrderInfo(@RequestBody InsertOrderInfoDTO dto) {
-        if (dto == null || dto.getCustomerName() == null
-                || dto.getDetail() == null || dto.getOrderPrice() == null) {
-            return new ReturnObject(ErrInfo.PARAMETER_ERROR);
-        }
-        try {
-            // 解析订单detail
-            Map<String, Object> detailJSON = JSON.parseObject(dto.getDetail());
-            detailJSON.forEach((k, v) -> {
-                try {
-                    Integer.parseInt(k);
-                } catch (Exception e) {
-                    throw new BizException(ErrInfo.ORDERINFO_DETAIL_FORMAT_ERROR);
-                }
-                Map<String, Object> objV = (Map<String, Object>) v;
-                if(objV.size() != 2 || !objV.containsKey("number") || !objV.containsKey("price")) {
-                    throw new BizException(ErrInfo.ORDERINFO_DETAIL_FORMAT_ERROR);
-                }
-                // 默认状态为进行中(1)
-                objV.put("status", 1);
-                detailJSON.put(k, objV);
-                // 减少商品库存
-                commodityService.increaseOrDecreaseInventory(Integer.parseInt(k), - (Integer) objV.get("number"));
-            });
-            // 写入订单信息库
-            return new ReturnObject(true, orderInfoService.insertOrderInfo(dto.
-                    getCustomerName(), JSON.toJSONString(detailJSON), dto.getOrderPrice()), 0);
-        } catch (BizException e) {
-            return new ReturnObject(e);
-        }
     }
 }
